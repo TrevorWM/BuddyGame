a5deaa94e40bcdbb2a3eafb3d80b22e42b44e589
Trevor <trevor.meest@gmail.com>
2024-08-08 00:36:38 -0600

    Starting to add navigation to buddy

---
+++ scenes/buddy.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var movement_speed: float = 4.0
+@onready var navigation_agent: NavigationAgent3D = get_node("NavigationAgent3D")
+
+func _ready() -> void:
+	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * movement_speed
+	if navigation_agent.avoidance_enabled:
+		navigation_agent.velocity = new_velocity
+	else:
+		_on_velocity_computed(new_velocity)
+
+func _on_velocity_computed(safe_velocity: Vector3):
+	velocity = safe_velocity
+	move_and_slide()
+
+
+func _on_navigation_agent_3d_navigation_finished():
+	set_movement_target(NavigationServer3D.map_get_closest_point(get_world_3d().navigation_map, $"../Player".global_position))
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		set_movement_target(body.global_position)
