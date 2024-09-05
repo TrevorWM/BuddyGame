d4aa541ecf1016150324b5d8d06209b8d5caf7bd
Trevor <trevor.meest@gmail.com>
2024-09-05 13:16:52 -0600

    Added dog buddy. Progress on tug of war

--- scripts/autoloads/globals.gd
+++ scripts/autoloads/globals.gd
@@ -11,3 +11,7 @@ enum STAT {
 	AFFECTION,
 	HUNGER,
 }
+
+var current_buddies: Array[BuddyResource] = [
+	preload("res://assets/resources/sloth_buddy.tres"),
+	preload("res://assets/resources/dog_buddy.tres")]
--- scripts/minigames/tug_of_war.gd
+++ scripts/minigames/tug_of_war.gd
@@ -3,26 +3,36 @@ extends Node3D
 
 @export var stat_check_timer: Timer
 
-var start_left_muscle: int = 5
-var start_left_stam: int = 10
+var left_buddy: BuddyResource
+var right_buddy: BuddyResource
 
-var start_right_muscle: int = 5
-var start_right_stam: int = 5
+var left_current_muscle: int
+var left_start_muscle: int
+var left_current_energy: int
+var left_start_energy: int
 
-var left_muscle: int = start_left_muscle
-var left_stam: int = start_left_stam
-
-var right_muscle: int = start_right_muscle
-var right_stam: int = start_right_stam
+var right_current_muscle: int
+var right_start_muscle: int
+var right_current_energy: int
+var right_start_energy: int
 
 var left_resting: bool = false
 var right_resting: bool = false
 var is_rolling: bool = false
+
 var rope_position: int = 50
+
 var left_modifier: int = 0
 
-func _ready():
-	stat_check_timer.start()
+var game_state: STATE = STATE.PREGAME
+var winner: String = ""
+
+enum STATE {
+	PREGAME,
+	RUNNING,
+	WINNER,
+	ENDGAME,
+}
 
 func _on_stat_check_timeout():
 	var left_roll: int = 0
@@ -31,30 +41,30 @@ func _on_stat_check_timeout():
 	is_rolling = true
 	
 	if not left_resting:
-		left_roll = randi_range(0, left_muscle + left_modifier)
-		left_stam -= 1
+		left_roll = randi_range(0, left_current_muscle + left_modifier)
+		left_current_energy -= 1
 	
 	if not right_resting:
-		right_roll = randi_range(0, right_muscle)
-		right_stam -= 1
+		right_roll = randi_range(0, right_current_muscle)
+		right_current_energy -= 1
 	
 	if left_resting:
-		left_stam += (1 + left_modifier)
+		left_current_energy += (1 + left_modifier)
 		left_roll = 0
-		if left_stam >= ceili(start_left_stam/2):
+		if left_current_energy >= ceili(float(left_start_energy)/2.0):
 			left_resting = false
 
 	if right_resting:
-		right_stam += (1 + randi_range(0,2))
+		right_current_energy += (1 + randi_range(0,2))
 		right_roll = 0
-		if right_stam >= ceili(start_right_stam/2):
+		if right_current_energy >= ceili(float(right_start_energy)/2.0):
 			right_resting = false
 		
-	print("Left: " + str(left_roll) + "/" + str(left_muscle + left_modifier))
-	print("Right: " + str(right_roll) + "/" + str(right_muscle))
+	print("Left: " + str(left_roll) + "/" + str(left_current_muscle + left_modifier))
+	print("Right: " + str(right_roll) + "/" + str(right_current_muscle))
 	
-	left_muscle = start_left_muscle
-	right_muscle = start_right_muscle
+	left_current_muscle = left_start_muscle
+	right_current_muscle = right_start_muscle
 	left_modifier = 0
 	
 	if left_roll > right_roll:
@@ -66,27 +76,63 @@ func _on_stat_check_timeout():
 	else:
 		print("Tie!")
 	
-	if left_stam < 1:
+	if left_current_energy < 1:
 		left_resting = true
 		
-	if right_stam < 1:
+	if right_current_energy < 1:
 		right_resting = true
 	
-	$PlaceholderLeft/Label3D.text = str(left_stam)
-	$PlaceholderRight/Label3D.text = str(right_stam)
-	$Label3D.text = "Rope Position\n" + str(rope_position)
+	$PlaceholderLeft/Label3D.text = str(left_current_energy)
+	$PlaceholderRight/Label3D.text = str(right_current_energy)
+	
+	if game_state == STATE.RUNNING:
+		$Label3D.text = "Rope Position\n" + str(rope_position)
 	
 	if rope_position < 1:
-		print("Left Wins!")
+		winner = left_buddy.buddy_name
+		game_state = STATE.WINNER
+		$Label3D.text = winner + " is the winner!"
 		return
 	elif rope_position > 99:
-		print("Right wins!")
+		winner = right_buddy.buddy_name
+		game_state = STATE.WINNER
+		$Label3D.text = winner + " is the winner!"
 		return
-		
-	stat_check_timer.start()
-	is_rolling = false
+	
+	if game_state == STATE.RUNNING:
+		stat_check_timer.start()
+		is_rolling = false
+	
 
 func _unhandled_input(event):
 	if event.is_action_released("cheer") and not is_rolling:
 		left_modifier += 1
+
+func init_buddy_stats() -> void:
+	left_start_muscle = left_buddy.muscle
+	left_current_muscle = left_buddy.muscle
+	left_current_energy = left_buddy.energy
+	left_start_energy = left_buddy.energy
 	
+	right_start_muscle = right_buddy.muscle
+	right_current_muscle = right_buddy.muscle
+	right_current_energy = right_buddy.energy
+	right_start_energy = right_buddy.energy
+
+func start_game() -> void:
+	init_buddy_stats()
+	stat_check_timer.start()
+	game_state = STATE.RUNNING
+
+func _on_sloth_button_pressed():
+	left_buddy = preload("res://assets/resources/sloth_buddy.tres")
+	right_buddy = preload("res://assets/resources/dog_buddy.tres")
+	$UI.hide()
+	start_game()
+
+
+func _on_dog_button_pressed():
+	left_buddy = Globals.current_buddies[1]
+	right_buddy = Globals.current_buddies[0]
+	$UI.hide()
+	start_game()
